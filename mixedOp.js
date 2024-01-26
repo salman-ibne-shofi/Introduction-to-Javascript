4584e150d31716e6d3e0853907d33cba43f9541a
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-01-27 02:32:01 +0600

    mathmetical operation shorthand

---
+++ mixedOp.js
@@ -0,0 +1,9 @@
+var first = 'Mobarok';
+var second = 'tobarok';
+
+var fullname = first + second;
+// console.log(fullname);
+// console.log('40'+2);
+console.log(40-'20');
+console.log('50'-10);
+console.log(10*'2');
