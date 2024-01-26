a558bb4af79a77ad8c35049f5262f5f581141fa5
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-01-26 17:48:21 +0600

    variable data types numaric string boolean

---
+++ types.js
@@ -0,0 +1,10 @@
+// string
+
+var name = "sadar uddin";
+var address = "Adar killa bandarban with some kata bon";
+var country = "Bangladesh";
+// boolean
+var isrich = true;
+var passed = false;
+console.log(typeof passed);
+console.log(typeof country);
