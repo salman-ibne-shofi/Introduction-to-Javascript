a1b349e164b6c3c190ab2b6530033e0483032192
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-01-27 01:45:32 +0600

    JS numbers

---
+++ number.js
@@ -0,0 +1,16 @@
+var orangeprice = 20;
+var chocolate = 0.5;
+// var applePrice = parseInt('22.5');
+var applePrice = parseFloat('22.5');
+
+// console.log(orangeprice);
+// console.log(applePrice);
+// console.log(orangeprice + applePrice)
+
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+// console.log(total.toFixed(1))
+const num = -50 / 0;;
+console.log(num);
